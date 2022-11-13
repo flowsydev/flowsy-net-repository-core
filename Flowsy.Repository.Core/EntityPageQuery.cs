e4c9ffe4c7def23c51b650fee48a03423dc4e637
Adrian Aviles <adrian8167e@gmail.com>
2022-11-12 17:57:10 -0700

    Add property for query result count
    The name of the property that holds the total entity count for a given paged query result.

--- Flowsy.Repository.Core/EntityPageQuery.cs
+++ Flowsy.Repository.Core/EntityPageQuery.cs
@@ -2,16 +2,23 @@ namespace Flowsy.Repository.Core;
 
 public abstract class EntityPageQuery<TCriteria> : IEntityPageQuery<TCriteria> where TCriteria : class
 {
-    protected EntityPageQuery() : this(1, long.MaxValue, false)
+    protected EntityPageQuery()
+        : this(1, long.MaxValue, false, string.Empty)
     {
     }
 
     protected EntityPageQuery(long pageNumber, long pageSize, bool countTotals)
+        : this (1, long.MaxValue, false, string.Empty)
+    {
+    }
+
+    protected EntityPageQuery(long pageNumber, long pageSize, bool countTotals, string totalProperty)
     {
         Criteria = default;
         PageNumber = pageNumber;
         PageSize = pageSize;
         CountTotals = countTotals;
+        TotalProperty = totalProperty;
     }
     
     public TCriteria? Criteria { get; set; }
@@ -19,6 +26,7 @@ public abstract class EntityPageQuery<TCriteria> : IEntityPageQuery<TCriteria> w
     public long PageNumber { get; set; }
     public long PageSize { get; set; }
     public bool CountTotals { get; set; }
+    public string TotalProperty { get; set; }
     
     public void Translate(out long offset, out long limit)
     {
