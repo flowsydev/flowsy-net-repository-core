9a6a47a709cbdc207ac244f7d84d0178c312cc92
Adrian Aviles <adrian8167e@gmail.com>
2022-11-13 18:01:58 -0700

    Improve pagination
    - Add HasMore property to EntityPageQueryResult.
    - Add XML documentation to EntityPageQuery and EntityPageQueryResult.

--- Flowsy.Repository.Core/EntityPageQuery.cs
+++ Flowsy.Repository.Core/EntityPageQuery.cs
@@ -1,8 +1,12 @@
 namespace Flowsy.Repository.Core;
 
+/// <summary>
+/// Represents a query to retrieve only a set of entities matching the specified criteria and pagination options.
+/// </summary>
+/// <typeparam name="TCriteria"></typeparam>
 public class EntityPageQuery<TCriteria> : IEntityPageQuery<TCriteria> where TCriteria : class
 {
-    public EntityPageQuery(
+   public EntityPageQuery(
         TCriteria? criteria = default,
         long pageNumber = 1,
         long pageSize = 50,
@@ -17,12 +21,36 @@ public class EntityPageQuery<TCriteria> : IEntityPageQuery<TCriteria> where TCri
         TotalCountProperty = totalCountProperty;
     }
 
+    /// <summary>
+    /// The criteria to match in order to retrieve the entities.
+    /// </summary>
     public TCriteria? Criteria { get; set; }
+    
+    /// <summary>
+    /// The number of the page of entities to retrieve.
+    /// </summary>
     public long PageNumber { get; set; }
+    
+    /// <summary>
+    /// The number of entities to retrieve.
+    /// </summary>
     public long PageSize { get; set; }
+    
+    /// <summary>
+    /// Whether or not to count the total of entities in the repository.
+    /// </summary>
     public bool CountTotal { get; set; }
+    
+    /// <summary>
+    /// The name of the property within the results holding the total entity count.
+    /// </summary>
     public string? TotalCountProperty { get; set; }
     
+    /// <summary>
+    /// Translates the PageNumber and PageSize properties to a pair of values based on offset and limit like the ones used in most relational databases.
+    /// </summary>
+    /// <param name="offset">The number of entities to skip.</param>
+    /// <param name="limit">The number of entities to retrieve.</param>
     public void Translate(out long offset, out long limit)
     {
         offset = (PageNumber - 1) * PageSize;
--- Flowsy.Repository.Core/EntityPageQueryResult.cs
+++ Flowsy.Repository.Core/EntityPageQueryResult.cs
@@ -1,5 +1,10 @@
 namespace Flowsy.Repository.Core;
 
+/// <summary>
+/// Represents the results of a query based on pagination.
+/// </summary>
+/// <typeparam name="TCriteria"></typeparam>
+/// <typeparam name="TResult"></typeparam>
 public class EntityPageQueryResult<TCriteria, TResult> 
     where TCriteria : class
     where TResult : class
@@ -11,15 +16,39 @@ public class EntityPageQueryResult<TCriteria, TResult>
         TotalResultCount = totalResultCount;
     }
 
+    /// <summary>
+    /// The original query associated with this result.
+    /// </summary>
     public EntityPageQuery<TCriteria> Query { get; }
     
+    /// <summary>
+    /// The list of entities matching to the criteria and pagination options. 
+    /// </summary>
     public IEnumerable<TResult> Results { get; }
     
-    public long PageSize => Results.Count();
-    public long? TotalResultCount { get; }
+    /// <summary>
+    /// The number of entities in the current result.
+    /// </summary>
+    public long ResultCount => Results.Count();
     
+    /// <summary>
+    /// If the value of Query.CountTotal is true, it contains the total number of entities in the repository for the specified query. 
+    /// </summary>
+    public long? TotalResultCount { get; }
+
+    /// <summary>
+    /// If the value of Query.CountTotal is true, it gets the total number of pages for the specified query.
+    /// </summary>
     public long? TotalPageCount => 
         Query.CountTotal && Query.PageSize > 0 && TotalResultCount.HasValue && TotalResultCount > 0
             ? (long) Math.Ceiling(TotalResultCount.Value / (decimal) Query.PageSize) 
             : default;
+    
+    /// <summary>
+    /// Indicates if more data can be retrieved by requesting the next page of entities. 
+    /// </summary>
+    public bool HasMore 
+        => Query.CountTotal && TotalResultCount.HasValue && TotalPageCount.HasValue 
+            ? Query.PageNumber < TotalPageCount
+            : Query.PageSize == ResultCount;
 }
