ec8bf8a79274e7fdbafe570ec43e4cba44d7506e
Adrian Aviles <adrian8167e@gmail.com>
2023-01-03 10:27:34 -0700

    Rename EntityPage properties
    Refactor EntityPage to give more appropriate names to its properties.

--- Flowsy.Repository.Core/EntityPage.cs
+++ Flowsy.Repository.Core/EntityPage.cs
@@ -1,19 +1,19 @@
 namespace Flowsy.Repository.Core;
 
 /// <summary>
-/// Represents the results of a query based on pagination.
+/// Represents the results of a query based on the given criteria and pagination options.
 /// </summary>
 /// <typeparam name="TCriteria">The type of criteria and pagination options.</typeparam>
-/// <typeparam name="TResult">The result of the query.</typeparam>
-public class EntityPage<TCriteria, TResult> 
+/// <typeparam name="TEntity">The type of entities in the page.</typeparam>
+public class EntityPage<TCriteria, TEntity> 
     where TCriteria : EntityPageCriteria
-    where TResult : class
+    where TEntity : class
 {
-    public EntityPage(TCriteria criteria, IEnumerable<TResult> results, long? totalResultCount)
+    public EntityPage(TCriteria criteria, IEnumerable<TEntity> entities, long? totalEntityCount)
     {
         Criteria = criteria;
-        Results = results;
-        TotalResultCount = totalResultCount;
+        Entities = entities;
+        TotalEntityCount = totalEntityCount;
     }
 
     /// <summary>
@@ -24,31 +24,31 @@ public class EntityPage<TCriteria, TResult>
     /// <summary>
     /// The list of entities matching to the criteria and pagination options. 
     /// </summary>
-    public IEnumerable<TResult> Results { get; }
+    public IEnumerable<TEntity> Entities { get; }
     
     /// <summary>
-    /// The number of entities in the current result.
+    /// The number of entities in the current page.
     /// </summary>
-    public long ResultCount => Results.Count();
+    public long EntityCount => Entities.Count();
     
     /// <summary>
-    /// If the value of Query.CountTotal is true, it contains the total number of entities in the repository for the specified query. 
+    /// If the value of Criteria.CountTotal is true, it contains the total number of entities in the repository for the specified criteria. 
     /// </summary>
-    public long? TotalResultCount { get; }
+    public long? TotalEntityCount { get; }
 
     /// <summary>
-    /// If the value of Query.CountTotal is true, it gets the total number of pages for the specified query.
+    /// If the value of Criteria.CountTotal is true, it gets the total number of pages available for the specified criteria.
     /// </summary>
     public long? TotalPageCount => 
-        Criteria.CountTotal && Criteria.PageSize > 0 && TotalResultCount.HasValue && TotalResultCount > 0
-            ? (long) Math.Ceiling(TotalResultCount.Value / (decimal) Criteria.PageSize) 
+        Criteria is {CountTotal: true, PageSize: > 0} && TotalEntityCount is > 0
+            ? (long) Math.Ceiling(TotalEntityCount.Value / (decimal) Criteria.PageSize) 
             : default;
     
     /// <summary>
     /// Indicates if more data can be retrieved by requesting the next page of entities. 
     /// </summary>
     public bool HasMore 
-        => Criteria.CountTotal && TotalResultCount.HasValue && TotalPageCount.HasValue 
+        => Criteria.CountTotal && TotalEntityCount.HasValue && TotalPageCount.HasValue 
             ? Criteria.PageNumber < TotalPageCount
-            : Criteria.PageSize == ResultCount;
+            : Criteria.PageSize == EntityCount;
 }
